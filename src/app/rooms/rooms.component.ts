27394c55d0b083f18893cbdafd7de788ef27b082
Hao Chen Liu <haochenliu@Haos-Laptop.local>
2023-04-06 17:24:00 +0200

    add rooms list component, learned using Input Output

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RoomsStatus, Room } from './rooms';
 
 @Component({
@@ -6,7 +6,7 @@ import { RoomsStatus, Room } from './rooms';
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
   hotelName = "Hilton"
 
   rooms: RoomsStatus = {
@@ -15,47 +15,53 @@ export class RoomsComponent {
     bookedRooms: 5
   }
 
-  roomsList: Room[] = [
-    {
-      roomNumber: 1,
-      roomType: "Deluxe King Room with Balcony",
-      amenities: "Free WiFi, Air conditioning, Flat-screen TV, Mini-bar",
-      price: 200,
-      photos: "https://unsplash.com/photos/xFv7VJxNkLg",
-      checkinTime: new Date("2022-05-01T14:00:00Z"),
-      checkoutTime: new Date("2022-05-03T12:00:00Z"),
-      rating: 4.2
-    },
-    {
-      roomNumber: 2,
-      roomType: "Standard Double Room",
-      amenities: "Free WiFi, Air conditioning, Flat-screen TV, Tea/coffee maker",
-      price: 120,
-      photos: "https://unsplash.com/photos/5O5AHfjM_vY",
-      checkinTime: new Date("2022-06-01T15:00:00Z"),
-      checkoutTime: new Date("2022-06-03T13:00:00Z"),
-      rating: 4.2
-    },
-    {
-      roomNumber: 3,
-      roomType: "Family Suite with Ocean View",
-      amenities: "Free WiFi, Air conditioning, Flat-screen TV, Kitchenette",
-      price: 350,
-      photos: "https://unsplash.com/photos/c7v0bSTQL0g",
-      checkinTime: new Date("2022-07-01T16:00:00Z"),
-      checkoutTime: new Date("2022-07-04T12:00:00Z"),
-      rating: 4.2
-    }
-  ];
+  roomsList: Room[] = []
   
 
   hideRooms = false
 
   constructor(){}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.roomsList = [
+      {
+        roomNumber: 1,
+        roomType: "Deluxe King Room with Balcony",
+        amenities: "Free WiFi, Air conditioning, Flat-screen TV, Mini-bar",
+        price: 200,
+        photos: "https://unsplash.com/photos/xFv7VJxNkLg",
+        checkinTime: new Date("2022-05-01T14:00:00Z"),
+        checkoutTime: new Date("2022-05-03T12:00:00Z"),
+        rating: 4.2
+      },
+      {
+        roomNumber: 2,
+        roomType: "Standard Double Room",
+        amenities: "Free WiFi, Air conditioning, Flat-screen TV, Tea/coffee maker",
+        price: 120,
+        photos: "https://unsplash.com/photos/5O5AHfjM_vY",
+        checkinTime: new Date("2022-06-01T15:00:00Z"),
+        checkoutTime: new Date("2022-06-03T13:00:00Z"),
+        rating: 4.2
+      },
+      {
+        roomNumber: 3,
+        roomType: "Family Suite with Ocean View",
+        amenities: "Free WiFi, Air conditioning, Flat-screen TV, Kitchenette",
+        price: 350,
+        photos: "https://unsplash.com/photos/c7v0bSTQL0g",
+        checkinTime: new Date("2022-07-01T16:00:00Z"),
+        checkoutTime: new Date("2022-07-04T12:00:00Z"),
+        rating: 4.2
+      }
+    ];
+  }
 
   toggle(){
     this.hideRooms = !this.hideRooms
+  }
+
+  selectRoom(room: Room) {
+    console.log(room)
   }
 }
